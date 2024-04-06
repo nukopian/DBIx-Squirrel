be3a4a08ee40cd782023c4e2513c226a4c14566a
nukopian <nukopian@users.noreply.github.com>
2024-04-06 21:50:58 +0100

    Updated lib/DBIx/Squirrel.pm
    - Add required copyright and licence info

--- lib/DBIx/Squirrel.pm
+++ lib/DBIx/Squirrel.pm
@@ -334,12 +334,21 @@ programmers who use them for similar purposes.
 
 =back
 
-=head1 AUTHOR
-
-(see .meta.json.enc)
-
 =head1 COPYRIGHT AND LICENSE
 
-(see .meta.json.enc)
+    Copyright (c) 2020-2024 Iain Campbell
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 =cut
