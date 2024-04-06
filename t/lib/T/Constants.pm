5e5448feb3c407751137ac7e855c6f84e43358ca
nukopian <nukopian@users.noreply.github.com>
2024-04-06 21:30:13 +0100

    Tidied up
    - Whitespace
    - Quoted exports
    - Nested final assignment more sensibly

--- t/lib/T/Constants.pm
+++ t/lib/T/Constants.pm
@@ -3,47 +3,46 @@ use warnings;
 
 package T::Constants;
 
+
 BEGIN {
-    require Exporter;
-    @T::Constants::ISA         = ( 'Exporter' );
-    %T::Constants::EXPORT_TAGS = (
-        all => [
-            qw/
-              $T_DATABASE
-              $T_DB_ATTR
-              $T_DB_DSN
-              $T_DB_USER
-              $T_DB_PASS
-              $T_LIB
-              $T_LIB_DATA
-              @T_DB_CONNECT_ARGS
-              /
-        ]
-    );
-    @T::Constants::EXPORT_OK = @{ $T::Constants::EXPORT_TAGS{ all } };
+	require Exporter;
+	@T::Constants::ISA = ( 'Exporter' );
+	%T::Constants::EXPORT_TAGS = (
+		all => [
+				'$T_DATABASE',
+				'$T_DB_ATTR',
+				'$T_DB_DSN',
+				'$T_DB_USER',
+				'$T_DB_PASS',
+				'$T_LIB',
+				'$T_LIB_DATA',
+				'@T_DB_CONNECT_ARGS',
+		],
+	);
+	@T::Constants::EXPORT_OK = @{ $T::Constants::EXPORT_TAGS{ all } };
 }
 
 use Test::Most;
 use Cwd 'realpath';
 
 our ( $T_LIB, $T_LIB_DATA, $T_DATABASE ) = do {
-    ( my $module = __PACKAGE__ )          =~ s/::/\//g;
-    ( my $lib    = realpath( __FILE__ ) ) =~ s/\/$module\.pm$//i;
-    ( "$lib", "$lib/data", "$lib/data/chinook.db" );
+	( my $module = __PACKAGE__ )          =~ s/::/\//g;
+	( my $lib    = realpath( __FILE__ ) ) =~ s/\/$module\.pm$//i;
+	( "$lib", "$lib/data", "$lib/data/chinook.db" );
 };
 
-our ( $T_DB_DSN, $T_DB_USER, $T_DB_PASS, $T_DB_ATTR ) = (
-    our @T_DB_CONNECT_ARGS = (
-        "dbi:SQLite:dbname=$T_DATABASE",
-        "",
-        "",
-        {
-            AutoCommit     => 0,
-            PrintError     => 0,
-            RaiseError     => 1,
-            sqlite_unicode => 1,
-        }
-    )
+our @T_DB_CONNECT_ARGS = (
+	our ( $T_DB_DSN, $T_DB_USER, $T_DB_PASS, $T_DB_ATTR ) = (
+		"dbi:SQLite:dbname=$T_DATABASE",
+		"",
+		"",
+		{
+		  AutoCommit     => 0,
+		  PrintError     => 0,
+		  RaiseError     => 1,
+		  sqlite_unicode => 1,
+		},
+	)
 );
 
 1;
