2aa7cccc11fc5f748df4e47cb530adf9208a7a5b
nukopian <nukopian@users.noreply.github.com>
2024-04-12 23:46:38 +0100

    Reformat code

--- lib/DBIx/Squirrel/dr.pm
+++ lib/DBIx/Squirrel/dr.pm
@@ -1,4 +1,4 @@
-package    # hide from PAUSE
+package                                                                                                                            # hide from PAUSE
   DBIx::Squirrel::dr;
 use strict;
 use warnings;
@@ -11,45 +11,39 @@ BEGIN {
 }
 
 {
-    my $r;
+    ( my $r = __PACKAGE__ ) =~ s/::\w+$//;
 
-    sub ROOT_CLASS
-    {
-        ( $r = __PACKAGE__ ) =~ s/::\w+$// unless defined $r;
-        return wantarray ? ( RootClass => $r ) : $r;
+    sub ROOT_CLASS {
+        return $r unless wantarray;
+        return RootClass => $r;
     }
 }
 
-sub _is_attr
-{
+sub _is_attr {
     return UNIVERSAL::isa( $_[0], 'HASH' ) ? $_[0] : undef;
 }
 
-sub _is_dbh
-{
+sub _is_dbh {
     return UNIVERSAL::isa( $_[0], 'DBI::db' ) ? $_[0] : undef;
 }
 
-sub connect
-{
+sub connect {
     return _is_dbh( $_[1] ) ? &connect_clone : shift->DBI::connect(
           ( @_ && _is_attr( $_[$#_] ) )
         ? ( @_[ 0 .. $#_ - 1 ], { %{ $_[$#_] }, __PACKAGE__->ROOT_CLASS } )
-        : ( @_,                 { __PACKAGE__->ROOT_CLASS } )
+        : ( @_, { __PACKAGE__->ROOT_CLASS } )
     );
 }
 
-sub connect_cached
-{
+sub connect_cached {
     return shift->DBI::connect_cached(
           ( @_ && _is_attr( $_[$#_] ) )
         ? ( @_[ 0 .. $#_ - 1 ], { %{ $_[$#_] }, __PACKAGE__->ROOT_CLASS } )
-        : ( @_,                 { __PACKAGE__->ROOT_CLASS } )
+        : ( @_, { __PACKAGE__->ROOT_CLASS } )
     );
 }
 
-sub connect_clone
-{
+sub connect_clone {
     return undef unless _is_dbh( $_[1] );
     $_[1]->clone(
           ( @_ && _is_attr( $_[$#_] ) )
