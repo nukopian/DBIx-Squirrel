28bf7c7a1bfd52ca6a783078077cb9ea0bb56bf1
nukopian <nukopian@users.noreply.github.com>
2024-08-05 22:08:00 +0100

    Add import method and more tests
    Creates and exports named subroutines that enable us to address principal entities by name.

--- lib/DBIx/Squirrel.pm
+++ lib/DBIx/Squirrel.pm
@@ -2,6 +2,10 @@ package DBIx::Squirrel;
 use strict;
 use warnings;
 
+use Carp 'croak';
+use Scalar::Util 'reftype';
+use Sub::Name;
+
 BEGIN {
     our $VERSION               = '1.0.0_210925';
     our @ISA                   = 'DBI';
@@ -33,6 +37,62 @@ BEGIN {
     *BUF_MULT        = *DBIx::Squirrel::it::BUF_MULT;
     *BUF_MAXROWS     = *DBIx::Squirrel::it::BUF_MAXROWS;
     *NORMALIZE_SQL   = *NORMALISE_SQL;
+}
+
+sub import {
+    no strict 'refs';
+
+    my $class  = shift;
+    my $caller = caller;
+
+    for my $name (@_) {
+        my $symbol = $class . '::' . $name;
+        unless ( defined &{$symbol} ) {
+            *{$symbol} = subname(
+                $symbol => sub {
+                    if (@_) {
+                        if ( UNIVERSAL::isa( $_[0], $class . '::db' ) ) {
+                            ${$symbol} = shift;
+                        } else {
+                            if ( UNIVERSAL::isa( $_[0], $class . '::st' ) ) {
+                                ${$symbol} = shift;
+                            } elsif ( UNIVERSAL::isa( $_[0], $class . '::it' ) ) {
+                                ${$symbol} = shift;
+                            } elsif ( UNIVERSAL::isa( $_[0], $class . '::rs' ) ) {
+                                ${$symbol} = shift;
+                            } else {
+
+                                # The underlying statement maye take no
+                                # parameters, in which case there are none
+                                # to pass. To coerce this function into
+                                # executing the statement (rather than
+                                # just returning its handle), allow any
+                                # parameters to be passed inside an
+                                # anonymous array, which may be empty
+                                # to signal that statement execution
+                                # is required.
+                                #
+                                if (@_) {
+                                    if ( @_ == 1 && ref $_[0] && reftype( $_[0] ) eq 'ARRAY' ) {
+                                        ${$symbol}->execute( @{ +shift } );
+                                    } else {
+                                        ${$symbol}->execute(@_);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    return ${$symbol};
+                }
+            );
+        }
+
+        unless ( defined &{ $caller . '::' . $name } ) {
+            *{ $caller . '::' . $name } = *{$symbol};
+        }
+    }
+    return $class;
 }
 
 1;
