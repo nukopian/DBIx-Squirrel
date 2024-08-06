cb77d31b5d8b540bcc914f9d544c8e88d74a8275
nukopian <nukopian@users.noreply.github.com>
2024-08-06 19:20:51 +0100

    Formatting

--- lib/DBIx/Squirrel/dr.pm
+++ lib/DBIx/Squirrel/dr.pm
@@ -15,28 +15,35 @@ BEGIN {
 
     sub RootClass {
         return $root unless wantarray;
+
         return RootClass => $root;
     }
 }
 
 sub connect {
     goto &_clone_connection if UNIVERSAL::isa( $_[1], 'DBI::db' );
+
     my $invocant   = shift;
     my $attributes = @_ && UNIVERSAL::isa( $_[$#_], 'HASH' ) ? pop : {};
+
     return $invocant->DBI::connect( @_, { %{$attributes}, __PACKAGE__->RootClass } );
 }
 
 sub connect_cached {
     my $invocant   = shift;
     my $attributes = @_ && UNIVERSAL::isa( $_[$#_], 'HASH' ) ? pop : {};
+
     return $invocant->DBI::connect_cached( @_, { %{$attributes}, __PACKAGE__->RootClass } );
 }
 
 sub _clone_connection {
     my $invocant = shift;
+
     return unless UNIVERSAL::isa( $_[0], 'DBI::db' );
+
     my $connection = shift;
     my $attributes = @_ && UNIVERSAL::isa( $_[$#_], 'HASH' ) ? pop : {};
+
     return $connection->clone( { %{$attributes}, __PACKAGE__->RootClass } );
 }
 
