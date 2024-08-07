bcdcebbc80dbb3287519d12e385e1f66bd8e09b2
nukopian <nukopian@users.noreply.github.com>
2024-08-08 00:08:08 +0100

    Add boilerplate ready for distribution

--- lib/DBIx/Squirrel.pm
+++ lib/DBIx/Squirrel.pm
@@ -1,463 +1,105 @@
 package DBIx::Squirrel;
+
+use 5.006;
 use strict;
 use warnings;
-use constant E_BAD_ENT_BIND     => 'May only bind a database connetion handle, statement handle, or iterator';
-use constant E_BAD_ENT_TYPE     => 'May only address a statement handle or iterator';
-use constant E_EXP_HASH_ARR_REF => 'Expected a reference to a HASH or ARRAY';
-
-use Scalar::Util 'reftype';
-use Sub::Name;
-
-BEGIN {
-    our $VERSION = '1.0.0_20240807';
-    $VERSION = eval $VERSION;
-}
-
-=pod
-
-=encoding UTF-8
 
 =head1 NAME
 
-DBIx::Squirrel - A module for working with databases
+DBIx::Squirrel - The great new DBIx::Squirrel!
 
 =head1 VERSION
 
-1.0.0_20240807
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
 
 =head1 SYNOPSIS
 
-    # Simply use the package.
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
 
     use DBIx::Squirrel;
 
-    $dbh = DBIx::Squirrel->connect($dsn, $user, $pass, \%attr);
-    $sth = $dbh->prepare('SELECT * FROM product WHERE id = ?');
-    $res = $sth->execute('1001099');
-    $itr = $sth->iterate('1001099');
-    while ($row = $itr->next) {...}
+    my $foo = DBIx::Squirrel->new();
+    ...
 
-    # Or, use it and have it create and import helper functions that you
-    # can define at runtime use (and reuse) to interact with database
-    # connections, statements and iterators.
+=head1 EXPORT
 
-    use DBIx::Squirrel 'db', 'st', 'it';
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
 
-    db DBIx::Squirrel->connect($dsn, $user, $pass, \%attr);
-    st db->prepare('SELECT * FROM product WHERE id = ?');
-    $res = st->execute('1001099');
-    $res = st('1001099');  # Same as line above.
-    it st->iterate('1001099');
-    while ($row = it->next) {...}
+=head1 SUBROUTINES/METHODS
 
-    # Clone another database connection.
-
-    $dbi = DBI->connect($dsn, $user, $pass, \%attr);
-    $dbh = DBIx::Squirrel->connect($dbi);
-
-    # Prepare a statement object.
-
-    $sth = $dbh->prepare($statement, \%attr);
-    $sth = $dbh->prepare_cached($statement, \%attr, $if_active);
-
-    # Commonly used positional and named parameter placeholder schemes
-    # conveniently supported regardless of database driver in use.
-
-    $sth = $dbh->prepare('SELECT * FROM product WHERE id = ?');
-    $sth = $dbh->prepare('SELECT * FROM product WHERE id = ?1');
-    $sth = $dbh->prepare('SELECT * FROM product WHERE id = $1');
-    $sth = $dbh->prepare('SELECT * FROM product WHERE id = :1');
-    $sth = $dbh->prepare('SELECT * FROM product WHERE id = :id');
-
-    # Able to bind values to individual parameters for both positional
-    # and named placeholder schemes.
-
-    $sth->bind_param(1, '1001099');
-    $sth->bind_param(':id', '1001099');
-    $sth->bind_param('id', '1001099');
-
-    # Bind multiple values to parameters in a single statement.
-
-    $sth->bind( '1001099', ... );
-    $sth->bind( [ '1001099', ... ] );
-    $sth->bind( ':id' => '1001099', ... );
-    $sth->bind( id => '1001099', ... );
-    $sth->bind( { ':id' => '1001099', ... } );
-    $sth->bind( { id => '1001099', ... } );
-
-    # Or just have the statement handle's or iterator's "execute"
-    # method bind all values to parameters by passing it the same
-    # arguments you would pass to "bind".
-
-    $res = $obj->execute( '1001099', ... );
-    $res = $obj->execute( [ '1001099', ... ] );
-    $res = $obj->execute( ':id' => '1001099', ... );
-    $res = $obj->execute( id => '1001099', ... );
-    $res = $obj->execute( { ':id' => '1001099', ... } );
-    $res = $obj->execute( { id => '1001099', ... } );
-
-    # The database handle "do" method works as it does with DBI,
-    # with the exception that returns the result followed by the
-    # statement handle when called in list-context. This means
-    # we can use it to prepare and execute statements, before we
-    # fetch results. Be careful to use "undef" if passing named
-    # parameters in a hashref so they are not used as statement
-    # attributes. The new "do" is smart enough not to confuse
-    # other things as statement attributes.
-
-    ($res, $sth) = $dbh->do(
-        'SELECT * FROM product WHERE id = ?', '1001099'
-    );
-    ($res, $sth) = $dbh->do(
-        'SELECT * FROM product WHERE id = ?', ['1001099']
-    );
-    ($res, $sth) = $dbh->do(
-        'SELECT * FROM product WHERE id = :id', ':id' => '1001099'
-    );
-    ($res, $sth) = $dbh->do(
-        'SELECT * FROM product WHERE id = :id', id => '1001099'
-    );
-    ($res, $sth) = $dbh->do( # ------------ undef or \%attr
-        'SELECT * FROM product WHERE id = :id', undef,
-        { ':id' => '1001099'}
-    );
-    ($res, $sth) = $dbh->do( # ------------ undef or \%attr
-        'SELECT * FROM product WHERE id = :id', undef,
-        { id => '1001099' },
-    );
-
-    # Statement objects can create iterators using the "iterate"
-    # method (or its "it" alias). Use it as you would "execute"
-
-    $itr = $sth->iterate( '1001099' );
-    $itr = $sth->iterate(['1001099']);
-
-    $itr = $sth->iterate( '1001099' );
-    $itr = $sth->iterate(['1001099']);
-
-    $itr = $sth->iterate( '1001099' );
-    $itr = $sth->iterate(['1001099']);
-
-    $itr = $sth->iterate( '1001099' );
-    $itr = $sth->iterate(['1001099']);
-
-    $itr = $sth->iterate( '1001099' );
-    $itr = $sth->iterate(['1001099']);
-
-    $itr = $sth->iterate( ':id' => '1001099' );
-    $itr = $sth->iterate( id => '1001099' );
-
-    $itr = $sth->iterate( { ':id' => '1001099' } );
-    $itr = $sth->iterate( { id => '1001099' } );
-
-    # Using the iterators couldn't be easier!
-
-    @ary = ();
-    while ($row = $itr->next) {
-        push @ary, $row;
-    }
-
-    @ary = $itr->first;
-    push @ary, $_ while $itr->next;
-
-    @ary = $itr->first;
-    push @ary, $itr->remaining;
-
-    @ary = $itr->all;
-
-    $itr = $itr->reset;     # Repositions iterator at the start
-    $itr = $itr->reset({}); # Fetch rows as hashrefs
-    $itr = $itr->reset([]); # Fetch rows as arrayrefs
-
-    $row = $itr->single;
-    $row = $itr->single( id => '1001100' );
-    $row = $itr->single( { id => '1001100' } );
-    $row = $itr->find( id => '1001100' );
-    $row = $itr->find( { id => '1001100' } );
-
-    # A result set is just fancy subclass of the iterator. It will
-    # "bless" results, enabling us to get a column's value using an
-    # accessor methods, without ever having to worry about whether
-    # the row is a array or hash reference. While the accessor
-    # methods use lowercase names, they will access the column's
-    # value regardless of the case used.
-
-    $sth = $dbh->prepare('SELECT MediaTypeId, Name FROM media_types');
-    $res = $sth->rs;
-    while ($res->next) {
-        print $_->name, "\n";
-    }
-
-    # Iterators allow for the use of lambda functions to process
-    # each row just in time during iteration.
-
-    $it = $sth->iterate(
-        sub { $_->{Name} }
-    )->reset({});
-    print "$_\n" foreach $it->all;
-
-    # Lambdas may be chained.
-
-    $res = $sth->rs(
-        sub { $_->Name },
-        sub { "Media type: $_" },
-    );
-    print "$_\n" while $res->next;
-
-    print "$_\n" for $dbh->rs(
-        q/SELECT MediaTypeId, Name FROM media_types/,
-        sub { $_->Name },
-    )->all;
-
-    print "$_\n" for $dbh->select('media_types')->rs(
-        sub { $_->Name },
-    )->all;
+=head2 function1
 
 =cut
 
-use DBI                ();
-use DBIx::Squirrel::dr ();
-use DBIx::Squirrel::db ();
-use DBIx::Squirrel::st ();
-use DBIx::Squirrel::it ();
-use DBIx::Squirrel::rs ();
-use DBIx::Squirrel::rc ();
-use DBIx::Squirrel::util 'throw';
-
-BEGIN {
-    *err    = *DBI::err;
-    *errstr = *DBI::errstr;
-    *rows   = *DBI::rows;
-    *lasth  = *DBI::lasth;
-    *state  = *DBI::state;
-
-    *connect         = *DBIx::Squirrel::dr::connect;
-    *connect_cached  = *DBIx::Squirrel::dr::connect_cached;
-    *SQL_ABSTRACT    = *DBIx::Squirrel::db::SQL_ABSTRACT;
-    *DEFAULT_SLICE   = *DBIx::Squirrel::it::DEFAULT_SLICE;
-    *DEFAULT_MAXROWS = *DBIx::Squirrel::it::DEFAULT_MAXROWS;
-    *BUF_MULT        = *DBIx::Squirrel::it::BUF_MULT;
-    *BUF_MAXROWS     = *DBIx::Squirrel::it::BUF_MAXROWS;
-
-    our @ISA                   = ('DBI');
-    our $STRICT_PARAM_CHECKING = 0;
-    our $AUTO_FINISH_ON_ACTIVE = 1;
-    our $NORMALISE_SQL         = 1;
-    *NORMALIZE_SQL = *NORMALISE_SQL;
+sub function1 {
 }
 
-sub import {
-    no strict 'refs';
-
-    my $class  = shift;
-    my $caller = caller;
-
-    for my $name (@_) {
-        my $symbol = $class . '::' . $name;
-
-        unless ( defined &{$symbol} ) {
-            *{$symbol} = subname(
-                $symbol => sub {
-                    if (@_) {
-
-                        # By passing a database connection handle, statement
-                        # handle, iterator, or result set reference, we may
-                        # define an association between the function and an
-                        # entity at runtime.
-
-                        if (   UNIVERSAL::isa( $_[0], $class . '::db' )
-                            or UNIVERSAL::isa( $_[0], $class . '::st' )
-                            or UNIVERSAL::isa( $_[0], $class . '::it' ) )
-                        {
-                            ${$symbol} = shift;
-                            return ${$symbol};
-                        }
-
-                        throw E_BAD_ENT_BIND;
-                    }
-
-                    return unless defined ${$symbol};
-
-                    return ${$symbol} unless @_;
-
-                    # If the function reaches this point, we are
-                    # addressing the underlying entity, passing
-                    # parameters that are meaningful in that
-                    # context.
-
-                    if (   UNIVERSAL::isa( ${$symbol}, $class . '::st' )
-                        or UNIVERSAL::isa( ${$symbol}, $class . '::it' ) )
-                    {
-                        my @params = do {
-                            if ( @_ == 1 && ref $_[0] ) {
-
-                                # The underlying entity may take no
-                                # parameters, in which case there is
-                                # nothing meaningful that we can pass
-                                # to it. When no parameters are passed,
-                                # we expect this function to simply return
-                                # a reference to to the entity itself. If,
-                                # however, we wish to have the underlying
-                                # entity perform a contextually relevant
-                                # operation, we allow parameters to be
-                                # passed inside an anonymous array, which
-                                # may be empty as a signal to do just that.
-
-                                if ( reftype( $_[0] ) eq 'ARRAY' ) {
-                                    @{ +shift };
-                                } elsif ( reftype( $_[0] ) eq 'HASH' ) {
-                                    %{ +shift };
-                                } else {
-                                    throw E_EXP_HASH_ARR_REF;
-                                }
-                            } else {
-                                @_;
-                            }
-                        };
-
-                        return ${$symbol}->execute(@params);
-                    }
-
-                    throw E_BAD_ENT_TYPE;
-                }
-            );
-        }
-
-        # Export any relevant symbols to caller's namespace.
-
-        unless ( defined &{ $caller . '::' . $name } ) {
-            *{ $caller . '::' . $name } = *{$symbol};
-        }
-    }
-
-    return $class;
-}
-
-1;
-__END__
-
-=pod
-
-=encoding UTF-8
-
-=head1 DESCRIPTION
-
-B<Just what the world needs — another Perl package for working with databases!>
-
-C<DBIx::Squirrel> is a C<DBI> extension that serves as drop-in replacement for
-C<DBI>, while adding a few progressive enhancements to make some tasks much
-easier.
-
-=head1 DESIGN
-
-=head2 Compatibility
-
-DBIx-Squirrel's baseline behaviour is be like C<DBI>.
-
-A developer should be able to confidently replace C<use DBI> with
-C<use DBIx::Squirrel>, while expecting their script to behave just
-as it did before the change.
-
-DBIx-Squirrel's enhancements are designed to be low-friction, intuitive, and
-elective. Code using this package should behave like code using DBI, that is
-until deviation from standard behaviour is expected and invited.
-
-=head2 Ease of use
-
-An experienced user of DBI, or someone familiar with DBI's documentation,
-should be able to use DBIx-Squirrel without any issues.
-
-DBIx-Squirrel's enhancements are either additive or progressive. Experienced
-DBI and DBIx-Class programmers should find a cursory glance at the synopsis
-enough to get started in next to no time at all.
-
-The intention has been for DBIx-Squirrel was to occupy a sweet spot between DBI
-and DBIx-Class (though much closer to DBI).
-
-=head1 OVERVIEW
-
-=head2 Connecting to databases, and preparing statements
-
-=over
-
-=item *
-
-The C<connect> method continues to work as expected. It may also be invoked
-with a single argument (another database handle), if the intention is to clone
-that connection. This is particularly useful when cloning a standard DBI
-database object, since the resulting clone will be a DBI-Squirrel
-database object.
-
-=item *
-
-Both C<prepare> and C<prepare_cached> methods continue to work as expected,
-though passing a statement handle, instead of a statement in a string, results
-in that statement being cloned. Again, this is useful when the intention is to
-clone a standard DBI statement object in order to produce a DBIx-Squirrel
-statement object.
-
-=back
-
-=head2 Parameter placeholders, bind values, and iterators
-
-=over
-
-=item *
-
-Regardless of the database driver being used, DBIx-Squirrel provides full,
-baseline support for five parameter placeholder schemes (C<?>, C<?1>,
-C<$1>, C<:1>, C<:name>), offering a small degree of code portability to
-programmers with standard SQL, SQLite, PostgreSQL and Oracle backgrounds.
-
-=item *
-
-A statement object's C<bind_param> method will continue to work as expected,
-though its behaviour has been progressively enhanced. It now accommodates
-both C<bind_param(':name', 'value')> and C<bind_param('name', 'value')>
-calling styles, as well as the C<bind_param(1, 'value')> style for
-positional placeholders.
-
-=item *
-
-Statement objects have a new C<bind> method aimed at greatly streamlining
-the binding of values to statement parameters.
-
-=item *
-
-A statement object's C<execute> method will accept any arguments you would
-pass to the C<bind> method. It isn't really necessary to call C<bind> because
-C<execute> will take care of that.
-
-=item *
-
-DBIx-Squirrel iterators make the traversal of result sets simple and
-efficient, and these can be generated by using a statement object's
-C<iterate> method in place of C<execute>, with both methods taking
-the same arguments.
-
-=item *
-
-Some DBIx-Squirrel iterator method names (C<reset>, C<first>, C<next>,
-C<single>, C<find>, C<all>) may be familiar to other DBIx-Class
-programmers who use them for similar purposes.
-
-=back
-
-=head1 COPYRIGHT AND LICENSE
-
-    Copyright (c) 2020-2024 Iain Campbell
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+=head2 function2
 
 =cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Iain Campbell, C<< <cpanic at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dbix-squirrel at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-Squirrel>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc DBIx::Squirrel
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=DBIx-Squirrel>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/DBIx-Squirrel>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/DBIx-Squirrel>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2024 by Iain Campbell.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of DBIx::Squirrel
