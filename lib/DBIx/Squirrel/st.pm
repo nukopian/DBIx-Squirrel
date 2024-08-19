bb89ca30f651149228b842d379f8a63820468288
nukopian <nukopian@users.noreply.github.com>
2024-08-19 23:20:10 +0100

    chore: Renamed internal st.pm method

--- lib/DBIx/Squirrel/st.pm
+++ lib/DBIx/Squirrel/st.pm
@@ -16,7 +16,7 @@ use DBIx::Squirrel::util qw/throw whine/;
 use constant E_INVALID_PLACEHOLDER => 'Cannot bind invalid placeholder (%s)';
 use constant W_ODD_NUMBER_OF_ARGS  => 'Check bind values match placeholder scheme';
 
-sub _private_attributes {
+sub _private {
     my $self = shift;
     return
       unless ref($self);
