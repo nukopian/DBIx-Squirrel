a2330d090b4d10294c7589aea7607c38a058cc77
nukopian <nukopian@users.noreply.github.com>
2024-08-25 12:13:26 +0100

    chore: Updated falback version for local tests

--- lib/DBIx/Squirrel.pm
+++ lib/DBIx/Squirrel.pm
@@ -357,7 +357,7 @@ BEGIN {
     *DBIx::Squirrel::NORMALIZE_SQL                = *DBIx::Squirrel::util::NORMALISE_SQL;
 
     unless (defined $DBIx::Squirrel::VERSION) {
-        my $v = "1.3.0";
+        my $v = "1.3.2";
         *DBIx::Squirrel::VERSION = \$v;
     }
 }
