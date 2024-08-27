79ca5103b740caddd6cf4daebb8c26c6ed751ffe
nukopian <nukopian@users.noreply.github.com>
2024-08-27 17:09:16 +0100

    chore: Source reordering

--- lib/DBIx/Squirrel/util.pm
+++ lib/DBIx/Squirrel/util.pm
@@ -40,11 +40,77 @@ use constant E_EXP_STH       => 'Expected a statement handle';
 use constant E_EXP_REF       => 'Expected a reference to a HASH or ARRAY';
 use constant E_BAD_CB_LIST   => 'Expected a reference to a list of code-references, a code-reference, or undefined';
 
-our $NORMALISE_SQL = !!1;
+sub args_partition {
+    my $s = scalar(@_);
+    my $n = $s;
+    return [] unless $n;
+    while ($n) {
+        last unless UNIVERSAL::isa($_[$n - 1], 'CODE');
+        $n -= 1;
+    }
+    return [@_] if $n == 0;
+    return [], @_ if $n == $s;
+    return [@_[$n .. $#_]], @_[0 .. $n - 1];
+}
 
 # Perl versions older than 5.14 do not support ${^GLOBAL_PHASE}, so provide
 # a shim that works around that wrinkle.
 sub global_destruct_phase {Devel::GlobalDestruction::in_global_destruction()}
+
+#TODO These statement functions really need moving!
+sub statement_study {
+    local($_);
+    my($normal, $trimmed, $digest) = statement_normalise(@_);
+    return unless length($trimmed);
+    my %positions_to_params_map = do {
+        if (my @params = $trimmed =~ m{[\:\$\?]\w+\b}g) {
+            map {(1 + $_ => $params[$_])} 0 .. $#params;
+        }
+        else {
+            ();
+        }
+    };
+    return \%positions_to_params_map, $normal, $trimmed, $digest;
+}
+
+sub statement_normalise {
+    my $trimmed = statement_trim(@_);
+    my $normal  = $trimmed;
+    $normal =~ s{[\:\$\?]\w+\b}{?}g;
+    return $normal, $trimmed, sql_digest($trimmed);
+}
+
+sub statement_trim {
+    my $sth_or_sql = shift;
+    if (ref($sth_or_sql)) {
+        if (UNIVERSAL::isa($sth_or_sql, 'DBIx::Squirrel::st')) {
+            return sql_trim($sth_or_sql->_private_state->{OriginalStatement});
+        }
+        elsif (UNIVERSAL::isa($sth_or_sql, 'DBI::st')) {
+            return sql_trim($sth_or_sql->{Statement});
+        }
+        else {
+            throw(E_EXP_STH);
+        }
+    }
+    else {
+        return sql_trim($sth_or_sql);
+    }
+}
+
+memoize('sql_digest');
+
+sub sql_digest {sha256_base64(shift)}
+
+memoize('sql_trim');
+
+sub sql_trim {
+    my $sql = defined($_[0]) && !ref($_[0]) ? shift : '';
+    $sql        =~ s{\s+--\s+.*$}{}gm;
+    $sql        =~ s{^[[:blank:]\r\n]+}{}gm;
+    $sql        =~ s{[[:blank:]\r\n]+$}{}gm;
+    return $sql =~ m/\S/ ? $sql : '';
+}
 
 sub throw {
     Carp::confess do {
@@ -68,73 +134,6 @@ sub whine {
             'Unhelpful warning issued';
         }
     };
-}
-
-sub statement_study {
-    local($_);
-    my($normal, $trimmed, $digest) = statement_normalise(@_);
-    return unless length($trimmed);
-    my %positions_to_params_map = do {
-        if (my @params = $trimmed =~ m{[\:\$\?]\w+\b}g) {
-            map {(1 + $_ => $params[$_])} 0 .. $#params;
-        }
-        else {
-            ();
-        }
-    };
-    return \%positions_to_params_map, $normal, $trimmed, $digest;
-}
-
-sub statement_normalise {
-    my $trimmed = statement_trim(@_);
-    my $normal  = $trimmed;
-    $normal =~ s{[\:\$\?]\w+\b}{?}g if $NORMALISE_SQL;
-    return $normal, $trimmed, sql_digest($trimmed);
-}
-
-sub statement_trim {
-    my $sth_or_sql = shift;
-    if (ref($sth_or_sql)) {
-        if (UNIVERSAL::isa($sth_or_sql, 'DBIx::Squirrel::st')) {
-            return sql_trim($sth_or_sql->_private_state->{OriginalStatement});
-        }
-        elsif (UNIVERSAL::isa($sth_or_sql, 'DBI::st')) {
-            return sql_trim($sth_or_sql->{Statement});
-        }
-        else {
-            throw E_EXP_STH;
-        }
-    }
-    else {
-        return sql_trim($sth_or_sql);
-    }
-}
-
-memoize('sql_digest');
-
-sub sql_digest {sha256_base64(shift)}
-
-memoize('sql_trim');
-
-sub sql_trim {
-    my $sql = defined($_[0]) && !ref($_[0]) ? shift : '';
-    $sql        =~ s{\s+--\s+.*$}{}gm;
-    $sql        =~ s{^[[:blank:]\r\n]+}{}gm;
-    $sql        =~ s{[[:blank:]\r\n]+$}{}gm;
-    return $sql =~ m/\S/ ? $sql : '';
-}
-
-sub args_partition {
-    my $s = scalar(@_);
-    my $n = $s;
-    return [] unless $n;
-    while ($n) {
-        last unless UNIVERSAL::isa($_[$n - 1], 'CODE');
-        $n -= 1;
-    }
-    return [@_] if $n == 0;
-    return [], @_ if $n == $s;
-    return [@_[$n .. $#_]], @_[0 .. $n - 1];
 }
 
 1;
