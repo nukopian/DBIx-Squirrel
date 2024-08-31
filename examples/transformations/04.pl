45a05f7e6feab2e3179ea0ba13f9dd2a6db48a64
nukopian <nukopian@users.noreply.github.com>
2024-08-31 22:48:45 +0100

    chore: new examples

---
+++ examples/transformations/04.pl
@@ -0,0 +1,20 @@
+use DBIx::Squirrel;
+
+$dbh = DBIx::Squirrel->connect(
+    "dbi:SQLite:dbname=./t/data/chinook.db",
+    "",
+    "", {
+        RaiseError     => !!1,
+        sqlite_unicode => !!1,
+    },
+);
+
+$artist_names = $dbh->results(
+    "SELECT * FROM artists ORDER BY Name" => sub {
+        $_->Name;
+    }
+);
+
+print "$_\n" while $artist_names->next();
+
+$dbh->disconnect();
