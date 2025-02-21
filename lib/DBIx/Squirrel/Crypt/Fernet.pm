a3bc94cf88094a39e37ab7fc514d72aa4d1d7828
nukopian <nukopian@users.noreply.github.com>
2025-02-21 11:53:25 +0000

    refactor: improve POD

--- lib/DBIx/Squirrel/Crypt/Fernet.pm
+++ lib/DBIx/Squirrel/Crypt/Fernet.pm
@@ -130,160 +130,36 @@ const my $LEN_HDR        => 25;
 const my $LEN_DIGEST     => 32;
 const my $LEN_HDR_DIGEST => $LEN_HDR + $LEN_DIGEST;
 
-{
+# Calculate the age (seconds) of a token from its timestamp field.
+sub _age_sec {
     use bytes;
-
-    sub _age_sec {
-        my($token) = @_;
-        return time - unpack('V', reverse(substr($token, 1, 8)));
-    }
-
-    sub _timestamp {
-        local $_;
-        my $t = time();
-        my @p = map(substr(pack('I', ($t >> $_ * 8) & 0xFF), 0, 1), 0 .. 7);
-        return join('', reverse(@p));
-    }
+    my($token) = @_;
+    return time - unpack('V', reverse(substr($token, 1, 8)));
 }
 
+
+# Generate a timestamp field that can be embedded in a token.
+sub _timestamp {
+    use bytes;
+    local $_;
+    my $t = time();
+    my @p = map(substr(pack('I', ($t >> $_ * 8) & 0xFF), 0, 1), 0 .. 7);
+    return join('', reverse(@p));
+}
+
+
+# Generate a random 32-byte Fernet key.
 sub _rand_key {
     return Crypt::CBC->random_bytes(32);
 }
 
+
+# Encode a binary string as Base64 with padding.
 sub _pad_b64encode {
     my $b64 = urlsafe_b64encode(shift);
     return $b64 . '=' x (4 - length($b64) % 4);
 }
 
-=head2 EXPORTED FUNCTIONS
-
-=head3 C<Fernet>
-
-    $obj = Fernet();
-    $obj = Fernet($key);
-
-Returns a new C<DBIx::Squirrel::Crypt::Fernet> object. If no arguments are
-passed then a random key will be generated. Alternatively, a Base64-encoded
-key may be passed as the only argument.
-
-The blessed scalar reference representing the object wraps the decoded bytes
-of the key. Care should be taken not to display this binary value, but to use
-the C<to_string> method (or stringification) to encode it as Base64.
-
-=cut
-
-sub Fernet {
-    return __PACKAGE__->new(@_);
-}
-
-=head3 C<generatekey>
-
-    $key = generatekey();
-
-Returns a Base64-encoded randomly-generated key.
-
-=head3 C<encrypt>
-
-    $token = encrypt($key, $message);
-
-Encrypts a message, returning a Base64-encode token.
-
-=head3 C<decrypt>
-
-    $message = decrypt($key, $token);
-    $message = decrypt($key, $token, $ttl);
-
-Decrypts the Base64-encoded token and returnsthe decrypted message. If
-the token could not be decrypted or it has expired, then C<undef> will
-be returned.
-
-=head3 C<verify>
-
-    $bool = verify($key, $token);
-    $bool = verify($key, $token, $ttl);
-
-Returns true if the token was encrypted using the same key and, if the
-C<$ttl> (time-to-live) argument is supplied, has not expired.
-
-=head3 The legacy C<Crypt::Fernet> interface
-
-At the time I wanted to use Wan Leung Wong's C<Crypt::Fernet> package, it had
-a few testing failures and would not build. I'm pretty sure the C<Crypt::CBC>
-dependency introduced a breaking change. I did submit a fix, but deployment
-and communication have been problematic. It has probably been fixed by now,
-but I have decided to rework the original package, extend the interface,
-and have kept this namespace active. Nevertheless, the lion's share of the
-credit should go to the author of the original work.
-
-The original C<Crypt::Fernet> package exported four functions as its primary
-public interface, and this package also offers the same:
-
-=over
-
-=item * C<fernet_decrypt>
-
-=item * C<fernet_genkey>
-
-=item * C<fernet_encrypt>
-
-=item * C<fernet_verify>
-
-=back
-
-=cut
-
-=head3 C<fernet_genkey>
-
-    $key = fernet_genkey();
-
-Returns a Base64-encoded randomly-generated key.
-
-=cut
-
-sub fernet_genkey {
-    goto &generatekey;
-}
-
-=head3 C<fernet_encrypt>
-
-    $token = fernet_encrypt($key, $message);
-
-Encrypts a message, returning a Base64-encode token.
-
-=cut
-
-sub fernet_encrypt {
-    goto &encrypt;
-}
-
-=head3 C<fernet_decrypt>
-
-    $message = fernet_decrypt($key, $token);
-    $message = fernet_decrypt($key, $token, $ttl);
-
-Decrypts the Base64-encoded token and returnsthe decrypted message. If
-the token could not be decrypted or it has expired, then C<undef> will
-be returned.
-
-=cut
-
-sub fernet_decrypt {
-    goto &decrypt;
-}
-
-=head3 C<fernet_verify>
-
-    $bool = fernet_verify($key, $token);
-    $bool = fernet_verify($key, $token, $ttl);
-
-Returns true if the token was encrypted using the same key and, if the
-C<$ttl> (time-to-live) argument is supplied, has not expired.
-
-=cut
-
-sub fernet_verify {
-    goto &verify;
-}
 
 =head2 METHODS
 
@@ -292,13 +168,15 @@ sub fernet_verify {
     $obj = DBIx::Squirrel::Crypt::Fernet->new();
     $obj = DBIx::Squirrel::Crypt::Fernet->new($key);
 
-Returns a new C<DBIx::Squirrel::Crypt::Fernet> object. If no arguments are
-passed then a random key will be generated. Alternatively, a Base64-encoded
-key may be passed as the only argument.
+A constructor (also see L<Fernet>).
 
-The blessed scalar reference representing the object wraps the decoded bytes
-of the key. Care should be taken not to display this binary value, but to use
-the C<to_string> method (or stringification) to encode it as Base64.
+If no arguments are passed then a random 32-byte Fernet key is generated. If
+a Base64-encoded key is passed then it will be decoded and its signing and
+encryption key fields extracted.
+
+Take care never to display the binary signing and extraction keys, but to use
+the C<to_string> method (or stringification) to recombine them into a Base64-
+encoded Fernet key.
 
 =cut
 
@@ -312,6 +190,7 @@ sub new {
     return bless $self, $class;
 }
 
+
 =head3 C<generatekey>
 
     $key = $obj->generatekey();
@@ -324,6 +203,7 @@ Returns a Base64-encoded randomly-generated key.
 sub generatekey {
     return _pad_b64encode(_rand_key());
 }
+
 
 =head3 C<encrypt>
 
@@ -358,14 +238,15 @@ sub encrypt {
     return _pad_b64encode($t . hmac_sha256($t, $signing_key));
 }
 
+
 =head3 C<decrypt>
 
     $message = $obj->decrypt($token);
     $message = $obj->decrypt($token, $ttl);
 
-Decrypts the Base64-encoded token and returnsthe decrypted message. If
-the token could not be decrypted or it has expired, then C<undef> will
-be returned.
+Returns the decrypted message, or C<undef> if the token could not be
+decrypted. If a time-to-live (seconds) is specified (C<$ttl>) then a
+further check is made to ensure that the token has not expired.
 
 =cut
 
@@ -393,13 +274,15 @@ sub decrypt {
     )->decrypt($ciphertext);
 }
 
+
 =head3 C<verify>
 
     $bool = $obj->verify($token);
     $bool = $obj->verify($token, $ttl);
 
-Returns true if the token was encrypted using the same key and, if the
-C<$ttl> (time-to-live) argument is supplied, has not expired.
+Returns true if the token was signed using the same signing key as that
+embedded in the Fernet key. If a time-to-live (seconds) is specified (C<$ttl>)
+then a further check is made to ensure that the token has not expired.
 
 =cut
 
@@ -419,6 +302,7 @@ sub verify {
     return $digest eq hmac_sha256($t, $signing_key);
 }
 
+
 =head3 C<to_string>
 
     $key = $obj->to_string();
@@ -431,6 +315,165 @@ Returns the Base64-encoded key.
 sub to_string {
     my($self) = @_;
     return _pad_b64encode(join('', @{$self}{qw(signing_key encrypt_key)}));
+}
+
+
+=head2 EXPORTS
+
+This package exports nothing by default.
+
+=head3 C<Fernet>
+
+    $obj = Fernet();
+    $obj = Fernet($key);
+
+Alternative constructor (also see L<new>).
+
+Returns a new C<DBIx::Squirrel::Crypt::Fernet> object.
+
+If no arguments are passed then a random 32-byte Fernet key is generated. If
+a Base64-encoded key is passed then it will be decoded and its signing and
+encryption key fields extracted.
+
+Take care never to display the binary signing and extraction keys, but to use
+the C<to_string> method (or stringification) to recombine them into a Base64-
+encoded Fernet key.
+
+=cut
+
+sub Fernet {
+    return __PACKAGE__->new(@_);
+}
+
+
+=head3 C<generatekey>
+
+    $key = generatekey();
+
+Returns a Base64-encoded randomly-generated key.
+
+=head3 C<encrypt>
+
+    $token = encrypt($key, $message);
+
+Encrypts a message, returning a Base64-encode token.
+
+While a Base64-encoded key may be passed as the first argument, it would be
+more efficient to call the "two-faced" C<encrypt> as a method on a Fernet
+object to avoid the repeated overhead of decoding and parsing-out the signing
+and encryption keys.
+
+=head3 C<decrypt>
+
+    $message = decrypt($key, $token);
+    $message = decrypt($key, $token, $ttl);
+
+Returns the decrypted message, or C<undef> if the token could not be
+decrypted. If a time-to-live (seconds) is specified (C<$ttl>) then a
+further check is made to ensure that the token has not expired.
+
+While a Base64-encoded key may be passed as the first argument, it would be
+more efficient to call the "two-faced" C<decrypt> as a method on a Fernet
+object to avoid the repeated overhead of decoding and parsing-out the signing
+and encryption keys.
+
+=head3 C<verify>
+
+    $bool = verify($key, $token);
+    $bool = verify($key, $token, $ttl);
+
+Returns true if the token was signed using the same signing key as that
+embedded in the Fernet key. If a time-to-live (seconds) is specified (C<$ttl>)
+then a further check is made to ensure that the token has not expired.
+
+While a Base64-encoded key may be passed as the first argument, it would be
+more efficient to call the "two-faced" C<verify> as a method on a Fernet
+object to avoid the repeated overhead of decoding and parsing-out the signing
+and encryption keys.
+
+=cut
+
+
+=head2 LEGACY C<Crypt::Fernet> INTERFACE
+
+At the time I wanted to use Wan Leung Wong's C<Crypt::Fernet> package, it had
+a few testing failures and would not build. I'm pretty sure the C<Crypt::CBC>
+dependency introduced a breaking change. I did submit a fix, but deployment
+and communication have been problematic. It has probably been fixed by now,
+but I have decided to rework the original package, extend the interface,
+and have kept this namespace active. Nevertheless, the lion's share of the
+credit should go to the author of the original work.
+
+The original C<Crypt::Fernet> package exported four functions as its primary
+public interface, and this package does the same on request:
+
+=over
+
+=item * C<fernet_decrypt>
+
+=item * C<fernet_genkey>
+
+=item * C<fernet_encrypt>
+
+=item * C<fernet_verify>
+
+=back
+
+=head3 C<fernet_genkey>
+
+    $key = fernet_genkey();
+
+Returns a Base64-encoded randomly-generated key.
+
+=cut
+
+sub fernet_genkey {
+    goto &generatekey;
+}
+
+
+=head3 C<fernet_encrypt>
+
+    $token = fernet_encrypt($key, $message);
+
+Encrypts a message, returning a Base64-encode token.
+
+=cut
+
+sub fernet_encrypt {
+    goto &encrypt;
+}
+
+
+=head3 C<fernet_decrypt>
+
+    $message = fernet_decrypt($key, $token);
+    $message = fernet_decrypt($key, $token, $ttl);
+
+Returns the decrypted message, or C<undef> if the token could not be
+decrypted. If a time-to-live (seconds) is specified (C<$ttl>) then a
+further check is made to ensure that the token has not expired.
+
+=cut
+
+sub fernet_decrypt {
+    goto &decrypt;
+}
+
+
+=head3 C<fernet_verify>
+
+    $bool = fernet_verify($key, $token);
+    $bool = fernet_verify($key, $token, $ttl);
+
+Returns true if the token was signed using the same signing key as that
+embedded in the Fernet key. If a time-to-live (seconds) is specified (C<$ttl>)
+then a further check is made to ensure that the token has not expired.
+
+=cut
+
+sub fernet_verify {
+    goto &verify;
 }
 
 1;
